4db9d26fcc59e74d59c7b86edfde0b5ca4fd0f64
Brenon Parry <brenonparry@gmail.com>
2021-12-02 00:42:51 +0000

    Added functionality to prepare for user input

--- play.js
+++ play.js
@@ -1,24 +1,29 @@
 const net = require("net");
 const { connect } = require("./client");
-// establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: '165.227.47.243',
-//     port: 50541
-//   });
 
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-  
-//   conn.on('connect', () => {
-//     console.log(`I've connected!`);
-//   });
-//   conn.on('data', (data) => {
-//     console.log(data);
-//   });
+// setup interface to handle user input from stdin:
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
-//   return conn;
-// };
+
+
+const handleUserInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
