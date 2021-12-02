4b3dd5a5d542a161f0c2c8b5bf659ca581a19282
Brenon Parry <brenonparry@gmail.com>
2021-12-02 00:29:48 +0000

    added some hard-coded movement

--- client.js
+++ client.js
@@ -17,10 +17,69 @@ const connect = function () {
     conn.write('Name: BP');
   });
 
+  // conn.on('connect', () => {
+  //   conn.write('Imma YEET into the wall');
+  // });
+
+  conn.on('connect', () => {
+    // MOVE IN A CIRCLE:
+
+
+    // Timer to move UP//
+    setTimeout(() => {
+      let up = 0;
+      const moveUp = setInterval(() => {
+      up++
+      conn.write('Move: up');
+       if(up === 5) {
+        clearInterval(moveUp);
+      }
+    }, 500);
+    }, 0);
+    
+    // Timer to move RIGHT
+    setTimeout(() => {
+      let right = 0
+    const moveRight = setInterval(() => {
+      right++
+      conn.write('Move: right');
+      if(right === 5) {
+        clearInterval(moveRight);
+      }
+    }, 500);
+    }, 2500);
+
+    // Timer to move DOWN
+    setTimeout(() => {
+      let down = 0
+    const moveDown = setInterval(() => {
+      down++
+      conn.write('Move: down');
+      if(down === 5) {
+        clearInterval(moveDown);
+      }
+    }, 500);
+    }, 5000);
+
+    // Timer to move LEFT
+    setTimeout(() => {
+      let left = 0
+    const moveLeft = setInterval(() => {
+      left++
+      conn.write('Move: left');
+      if(left === 5) {
+        clearInterval(moveLeft);
+      }
+    }, 500);
+    }, 7500);
+
+  });
+
+  
+  
   conn.on('data', (data) => {
     console.log(data);
   });
-
   return conn;
 };
 
