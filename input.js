58098d2e7ed87f51dbdbbf5f976b80f1b62d029e
Brenon Parry <brenonparry@gmail.com>
2021-12-02 01:44:19 +0000

    Updated code to run off wasd key commands

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 
 
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,18 +13,34 @@ const setupInput = function () {
 };
 
 
-
-const handleUserInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', (key) => {
+const handleUserInput = function (key) {
     if (key === '\u0003') {
       process.exit();
-    }
-  });
+    };
+
+    if(key === 'w') {
+      connection.write("Move: up");
+    };
+    if(key === 's') {
+      connection.write("Move: down");
+    };
+    if(key === 'a') {
+      connection.write("Move: left");
+    };
+    if(key === 'd') {
+      connection.write("Move: right");
+    };
 };
+
 
 module.exports = {
   setupInput,
 };
+
+// if ('u0057') {
+//   console.log("checking key W: ", 'u0057')
+// };
+// w = u0057
+// a = u0041
+// s = u0053
+// d = u0044
