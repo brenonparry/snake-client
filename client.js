020cb93d54ae1ea496a48bba15293574257c6db3
Brenon Parry <brenonparry@gmail.com>
2021-12-02 01:43:55 +0000

    updated code to acount for keyboard movement commands from input.js

--- client.js
+++ client.js
@@ -17,63 +17,59 @@ const connect = function () {
     conn.write('Name: BP');
   });
 
+
+  //   // MOVE IN A CIRCLE:
   // conn.on('connect', () => {
-  //   conn.write('Imma YEET into the wall');
-  // });
-
-  conn.on('connect', () => {
-    // MOVE IN A CIRCLE:
-
-
-    // Timer to move UP//
-    setTimeout(() => {
-      let up = 0;
-      const moveUp = setInterval(() => {
-      up++
-      conn.write('Move: up');
-       if(up === 5) {
-        clearInterval(moveUp);
-      }
-    }, 500);
-    }, 0);
+  
+  //   // Timer to move UP//
+  //   setTimeout(() => {
+  //     let up = 0;
+  //     const moveUp = setInterval(() => {
+  //     up++
+  //     conn.write('Move: up');
+  //      if(up === 5) {
+  //       clearInterval(moveUp);
+  //     }
+  //   }, 500);
+  //   }, 0);
     
-    // Timer to move RIGHT
-    setTimeout(() => {
-      let right = 0
-    const moveRight = setInterval(() => {
-      right++
-      conn.write('Move: right');
-      if(right === 5) {
-        clearInterval(moveRight);
-      }
-    }, 500);
-    }, 2500);
+  //   // Timer to move RIGHT
+  //   setTimeout(() => {
+  //     let right = 0
+  //   const moveRight = setInterval(() => {
+  //     right++
+  //     conn.write('Move: right');
+  //     if(right === 5) {
+  //       clearInterval(moveRight);
+  //     }
+  //   }, 500);
+  //   }, 2500);
 
-    // Timer to move DOWN
-    setTimeout(() => {
-      let down = 0
-    const moveDown = setInterval(() => {
-      down++
-      conn.write('Move: down');
-      if(down === 5) {
-        clearInterval(moveDown);
-      }
-    }, 500);
-    }, 5000);
+  //   // Timer to move DOWN
+  //   setTimeout(() => {
+  //     let down = 0
+  //   const moveDown = setInterval(() => {
+  //     down++
+  //     conn.write('Move: down');
+  //     if(down === 5) {
+  //       clearInterval(moveDown);
+  //     }
+  //   }, 500);
+  //   }, 5000);
 
-    // Timer to move LEFT
-    setTimeout(() => {
-      let left = 0
-    const moveLeft = setInterval(() => {
-      left++
-      conn.write('Move: left');
-      if(left === 5) {
-        clearInterval(moveLeft);
-      }
-    }, 500);
-    }, 7500);
+  //   // Timer to move LEFT
+  //   setTimeout(() => {
+  //     let left = 0
+  //   const moveLeft = setInterval(() => {
+  //     left++
+  //     conn.write('Move: left');
+  //     if(left === 5) {
+  //       clearInterval(moveLeft);
+  //     }
+  //   }, 500);
+  //   }, 7500);
 
-  });
+  // });
 
   
   
